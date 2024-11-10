44484527d00d057f07fcef4a0e7bdfdb500854ae
equlibrino <69399726+equlibrino@users.noreply.github.com>
2024-11-10 20:53:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,178 +1,109 @@
-from playsound import playsound
-from gtts import gTTS
-import speech_recognition as sr
-import os
-import time
-from datetime import date, datetime
-import random
-from random import choice
-from pydub import AudioSegment
-import webbrowser
+from playsound import playsound       # playsound modülü, ses dosyalarını çalmak için kullanılır.
+from gtts import gTTS                 # gTTS (Google Text-to-Speech), metni sese çevirmek için kullanılır.
+import speech_recognition as sr       # speech_recognition modülü, konuşmayı metne çevirmek için kullanılır.
+import os                             # os modülü, işletim sistemi komutlarını çalıştırmak için kullanılır.
+import time                           # time modülü, zamanla ilgili işlemler için kullanılır.
+from datetime import date, datetime   # datetime modülü, tarih ve saat bilgilerini almak için kullanılır.
+import random                         # random modülü, rastgele seçimler yapmak için kullanılır.
+from random import choice             # choice fonksiyonu, listeden rastgele bir seçim yapmak için kullanılır.
+from pydub import AudioSegment        # pydub modülü, ses dosyaları ile çalışmak için kullanılır (bu kodda kullanılmamış).
+import webbrowser                     # webbrowser modülü, internet tarayıcısında sayfa açmak için kullanılır.
 
-r = sr.Recognizer()
+r = sr.Recognizer()                   # speech_recognition için bir 'Recognizer' nesnesi oluşturur; bu nesne, konuşmayı tanımak için kullanılır.
 
-def record(ask=False):
-    with sr.Microphone() as source:
+def record(ask=False):                # record fonksiyonu, kullanıcının sesli komutlarını kaydeder ve metne çevirir.
+    with sr.Microphone() as source:   # Mikrofonu 'source' olarak kullanmak için açar.
         if ask:
-            print(ask)
-        audio = r.listen(source)
-        voice = ""
+            print(ask)                # Eğer 'ask' parametresi varsa, bu mesajı ekrana yazdırır.
+        audio = r.listen(source)      # Mikrofon aracılığıyla ses dinler ve 'audio' olarak kaydeder.
+        voice = ""                    # voice değişkenini boş bir string olarak tanımlar.
         try:
-            voice = r.recognize_google(audio, language="tr-TR")
-        except sr.UnknownValueError:
+            voice = r.recognize_google(audio, language="tr-TR")  # Google'ın Türkçe dil modelini kullanarak sesi metne çevirir.
+        except sr.UnknownValueError:  # Ses tanınmazsa bu hata yakalanır.
             print("Dark: Anlayamadım")
-        except sr.RequestError:
+        except sr.RequestError:       # Google API'ye ulaşılamazsa bu hata yakalanır.
             print("Dark: Sistem çalışmıyor")
         return voice
 
-def response(voice):       
+def response(voice):                  # response fonksiyonu, kullanıcının komutuna göre yanıt verir.
        
-        #Sistemler
-        if voice == "sistemleri kapat":
+        # Sistemler
+        if voice == "sistemleri kapat":  # "sistemleri kapat" komutunda asistan kapanır.
             speak("Tabii Efendim, sistemleri kapatıyorum")
             exit()
         
-        elif voice == "uyku moduna geç":
+        elif voice == "uyku moduna geç": # "uyku moduna geç" komutunda asistan kapanır.
             speak("Tabii Efendim, uyku moduna geçiyorum")
             exit()
 
-        #Selamlaşma
-        elif "merhaba" in voice or "selam" in voice:
+        # Selamlaşma
+        elif "merhaba" in voice or "selam" in voice:   # "merhaba" veya "selam" içeren komutları yanıtlar.
          konuşma = ["Merhaba efendim, size nasıl yardımcı olabilirim ?",
-                "Selam efendim, size nasıl yardımcı olabilirim ?",
-                "İyi günler efendim, size nasıl yardımcı olabilirim ?",]
-         secim = choice(konuşma)
+                    "Selam efendim, size nasıl yardımcı olabilirim ?",
+                    "İyi günler efendim, size nasıl yardımcı olabilirim ?",]
+         secim = choice(konuşma)  # Rastgele bir selamlaşma mesajı seçer.
          speak(secim)
         
-        #Gün
-        elif voice == "hangi gündeyiz":
-            today = time.strftime("%A")
+        # Gün
+        elif voice == "hangi gündeyiz":  # Bugünün günü sorulursa, yanıt verir.
+            today = time.strftime("%A")  # Haftanın gününü İngilizce olarak alır.
             today.capitalize()
             if today == "Monday":
                 today = "Tabii Efendim, pazartesi günündeyiz"
-
             elif today == "Tuesday":
                 today = "Tabii Efendim, salı günündeyiz"
-
             elif today == "Wednesday":
                 today = "Tabii Efendim, çarşamba günündeyiz"
-
             elif today == "Thursday":
                 today = "Tabii Efendim, perşembe günündeyiz"
-
             elif today == "Friday":
                 today = "Tabii Efendim, cuma günündeyiz"
-
             elif today == "Saturday":
                 today = "Tabii Efendim, Cumartesi günündeyiz"
-
             elif today == "Sunday":
                 today = "Tabii Efendim, Pazar günündeyiz"
 
             speak(today)
 
-        #Saat
-        elif voice == "saat kaç":
+        # Saat
+        elif voice == "saat kaç":  # "saat kaç" sorusuna saati söyler.
            speak(datetime.now().strftime('Tabii Efendim, saat %H:%M'))
 
-        #Google Chrome 
-        elif voice == "arama yap":
+        # Google Chrome 
+        elif voice == "arama yap": # "arama yap" komutuyla Google'da arama yapar.
             speak("Tabii Efendim, ne aramak istersiniz ?")
-            search = record()
-            url = "https://www.google.com/search?q={}".format(search)
-            webbrowser.get().open(url)
+            search = record()      # Kullanıcıdan arama terimini kaydeder.
+            url = "https://www.google.com/search?q={}".format(search)  # Google arama URL'si oluşturur.
+            webbrowser.get().open(url)   # Tarayıcıda arama sayfasını açar.
             speak("Efendim {} hakkında bulduklarımı listeledim".format(search))
        
-        #Google Chrome 
-        elif voice == "youtube":
+        elif voice == "youtube":   # "youtube" komutuyla YouTube'da arama yapar.
             speak("Tabii Efendim, ne aramak istersiniz ?")
-            search = record()
-            url = "https://www.youtube.com/results?search_query={}".format(search)
-            webbrowser.get().open(url)
+            search = record()      # Kullanıcıdan arama terimini kaydeder.
+            url = "https://www.youtube.com/results?search_query={}".format(search)  # YouTube arama URL'si oluşturur.
+            webbrowser.get().open(url)   # Tarayıcıda arama sayfasını açar.
             speak("Efendim {} hakkında bulduklarımı listeledim".format(search))
 
-        #Toplu Programlar
-        elif "programları aç" in voice or "toplu aç" in voice:
-           os.startfile("C:\\Users\\mmust\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Discord Inc\\Discord")
-           os.startfile("C:\\Users\\mmust\\AppData\\Local\\Microsoft\\WindowsApps\\Spotify.exe")
-           os.startfile("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Google Chrome")
-           os.startfile("C:\\Users\\mmust\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Visual Studio Code\\Visual Studio Code")
-           speak("Tabii Efendim, ilgili sık kullandığınız programları açıyorum")
-        
-        #Programlar
-        elif "dosyalarım" in voice or "dosyalarım aç" in voice:
-           os.startfile("C:\\Users\\mmust\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Ramzan Emre Abra")
-           speak("Tabii Efendim, ilgili dosyanızı açıyorum")
-        
-        elif "discord" in voice or "discord aç" in voice:
-           os.startfile("C:\\Users\\mmust\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Discord Inc\\Discord")
-           speak("Tabii Efendim, ilgili programı açıyorum")
-        
-        elif "spotify" in voice or "spotify aç" in voice:
-           os.startfile("C:\\Users\\mmust\\AppData\\Local\\Microsoft\\WindowsApps\\Spotify.exe")
-           speak("Tabii Efendim, ilgili programı açıyorum") 
-        
-        elif "google chrome" in voice or "google chrome aç" in voice:
-           os.startfile("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Google Chrome")
-           speak("Tabii Efendim, ilgili programı açıyorum")
+        # Toplu Programlar
+        elif "programı aç" in voice or "" in voice:  # "programları aç" veya "" komutlarıyla belirtilen programları açar.
+           os.startfile("C:\\Users\\")  # Belirtilen klasörü açar.
 
-        elif "visual studio code" in voice or "visual studio code aç" in voice:
-           os.startfile("C:\\Users\\mmust\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Visual Studio Code\\Visual Studio Code")
-           speak("Tabii Efendim, ilgili programı açıyorum")
+           speak("Tabii Efendim, programı açıyorum")
         
-        #Oyun Launcher
-        elif "steam" in voice or "steam aç" in voice: 
-           os.startfile("C:\\Users\\mmust\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Steam\\Steam")
-           speak("Tabii Efendim, ilgili launcherı açıyorum")
-       
-        elif "epic games" in voice or "epic games aç" in voice: 
-           os.startfile("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Epic Games Launcher")
-           speak("Tabii Efendim, ilgili launcherı açıyorum")
-        
-        elif "gameloop" in voice or "gameloop aç" in voice: 
-           os.startfile("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Tencent Software\\Gameloop\\Gameloop")
-           speak("Tabii Efendim, ilgili launcherı açıyorum")
-        
-        #Oyunlar
-        elif "minecraft" in voice or "minecraft aç" in voice: 
-           os.startfile("E:\\Ramzan Emre Abra\\Oyunlar\\Minecraft.exe")
-           speak("Tabii Efendim, ilgili oyunu açıyorum")
-        
-        elif "rise" in voice or "rise aç" in voice: 
-           os.startfile("E:\\Ramzan Emre Abra\\Oyunlar\\RiseLauncher.exe")
-           speak("Tabii Efendim, ilgili oyunu açıyorum")
-        
-        elif "roblox" in voice or "roblox aç" in voice: 
-           os.startfile("C:\\Program Files (x86)\\Roblox\\Versions\\version-f98ef77f473148f6\\RobloxPlayerLauncher.exe")
-           speak("Tabii Efendim, ilgili oyunu açıyorum")
-        
-        elif "valorant" in voice or "volorant aç" in voice: 
-           os.startfile("E:\\Ramzan Emre Abra\\Oyunlar\\Riot Games\\Riot Client\\RiotClientServices.exe")
-           speak("Tabii Efendim, ilgili oyunu açıyorum")
-        
-        elif "zula" in voice or "zula aç" in voice: 
-           os.startfile("E:\\Ramzan Emre Abra\\Oyunlar\\Zula\\zula_launcher.exe")
-           speak("Tabii Efendim, ilgili oyunu açıyorum")
-        
-        elif "pubg mobile" in voice or "pubg mobile aç" in voice: 
-           os.startfile("E:\\Ramzan Emre Abra\\Oyunlar\\PUBG Mobile")
-           speak("Tabii Efendim, ilgili oyunu açıyorum")
+def speak(string):                    # speak fonksiyonu, verilen metni sese çevirir ve çalar.
+    tts = gTTS(text=string, lang="tr", slow=False)   # Metni Türkçe dilinde sese çevirir.
+    file = "answer.mp3"               # Ses dosyasını 'answer.mp3' olarak kaydeder.
+    tts.save(file)                    # Dosyayı kaydeder.
+    playsound(file)                   # Kaydedilen dosyayı çalar.
+    os.remove(file)                   # Dosyayı siler.
 
-def speak(string):
-    tts = gTTS(text=string, lang="tr", slow=False)
-    file = "answer.mp3"
-    tts.save(file)
-    playsound(file)
-    os.remove(file)
+# speak("Sistemler başlatıldı")       # Bu satır, sistem başlatıldığında bir selam sesi çalmak için kullanılmış ancak yorum satırına alınmış.
+playsound("gırıs.mp3")                # Başlangıçta 'GIRIS.mp3' dosyasını çalar.
 
-#speak("Sistemler başlatıldı")
-playsound("GIRIS.mp3")
-
-while True:
-    voice = record()
-    if voice != '':
-        voice = voice.lower()
-        print(voice.capitalize())
-        response(voice)
-
-   
+while True:                           # Sonsuz döngü başlatılır.
+    voice = record()                  # Kullanıcıdan sesli komut kaydedilir.
+    if voice != '':                   # Boş değilse (ses tanımlandıysa),
+        voice = voice.lower()         # Komut küçük harfe dönüştürülür.
+        print(voice.capitalize())     # Tanınan sesli komut ekrana yazdırılır.
+        response(voice)               # Komuta uygun yanıt fonksiyonu çağrılır
+#Equ
